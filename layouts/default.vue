a5dc8f295a1a7b4a05a35324c8dc54ffa5b443ee
Megvargian123 <mikekouyoumdjian@hotmail.com>
2025-09-20 19:59:41 +0300

    fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,12 +2,12 @@
   useSeoMeta({
     title: "Mike Kouyoumdjian | Web Developer",
     ogTitle: "Mike Kouyoumdjian | Web Developer",
-  description: "Explore João Pugsley's web development portfolio, showcasing a wide variety of creative and responsive websites.",
-  ogDescription: "Explore João Pugsley's web development portfolio, showcasing a wide variety of creative and responsive websites.",
+  description: "Dynamic Software Engineer with 5 years of experience in Vue.js, Nuxt, WordPress, and PHP. Proficient in unit testing using Jest and Vitest to ensure high-quality solutions. Experienced in developing cutting-edge web applications with seamless and intuitive user interfaces. Known for delivering clean, efficient code and solving complex challenges effectively. A proactive problem-solver with a strong collaborative mindset, focused on creating user-friendly solutions.",
+  ogDescription: "Dynamic Software Engineer with 5 years of experience in Vue.js, Nuxt, WordPress, and PHP. Proficient in unit testing using Jest and Vitest to ensure high-quality solutions. Experienced in developing cutting-edge web applications with seamless and intuitive user interfaces. Known for delivering clean, efficient code and solving complex challenges effectively. A proactive problem-solver with a strong collaborative mindset, focused on creating user-friendly solutions.",
   keywords: "web development, web design, portfolio, Mike Kouyoumdjian Pugsley, João Pugsley, front-end development, back-end development, responsive design",
     viewport: "width=device-width, initial-scale=1",
     ogType: "website",
-    ogUrl: "https://joaopugsley.dev",
+    ogUrl: "https://mike-kouyoumdjian.netlify.app/",
     ogSiteName: "Mike Kouyoumdjian | Web Developer"
   })
 </script>
